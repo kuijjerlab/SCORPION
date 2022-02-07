8e9305e873529575a4aa6e871bd33b0d391607e5
Daniel Osorio <dcosorioh@tamu.edu>
2022-02-07 13:40:18 -0600

    Create updateDiagonal.R

---
+++ R/updateDiagonal.R
@@ -0,0 +1,11 @@
+update.diagonal <- function(diagMat, num, alpha, step) {
+  seqs = seq(1, num * num, num + 1)
+  diagMat[seqs] = NaN
+
+  diagstd = rowSds(diagMat, na.rm = TRUE) * sqrt((num - 2) / (num - 1))
+
+  diagMat[seqs] = diagstd * num * exp(2 * alpha * step)
+
+  return(diagMat)
+
+}
