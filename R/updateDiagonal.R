a0e7206849aa31339b43e29106fd5f37fd733cf7
Daniel Osorio <d.osorio@me.com>
2026-02-20 16:58:51 -0600

    Refactor update.diagonal function to improve diagonal assignment and GPU support

--- R/updateDiagonal.R
+++ R/updateDiagonal.R
@@ -1,7 +1,12 @@
 update.diagonal <- function(diagMat, num, alpha, step) {
-  seqs <- seq(1, num * num, num + 1)
-  diagMat[seqs] <- NaN
+  isGPU <- grepl('gpu', class(diagMat))
+  diagMat <- as.matrix(diagMat)
+  diag(diagMat) <- NaN
   diagstd <- rowSds(diagMat, na.rm = TRUE) * sqrt((num - 2) / (num - 1))
-  diagMat[seqs] <- diagstd * num * exp(2 * alpha * step)
+  diag(diagMat) <- diagstd * num * exp(2 * alpha * step)
+  if(isGPU){
+    gpuMatrix <- getFromNamespace("gpuMatrix", "gpuR")
+    diagMat <- gpuMatrix(diagMat)
+  }
   return(diagMat)
 }
